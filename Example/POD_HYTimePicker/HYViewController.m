20962aa39b3761f4dde321de3d2a6dbd3714899e
HanYong <1152829171@qq.com>
2020-03-18 17:28:26 +0800

    demo

--- Example/POD_HYTimePicker/HYViewController.m
+++ Example/POD_HYTimePicker/HYViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "HYViewController.h"
+#import "HYTimePickerView.h"
 
-@interface HYViewController ()
+@interface HYViewController ()<TimePickerViewDelegate>
+@property (nonatomic, strong) UILabel *label;
 
 @end
 
@@ -18,7 +20,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(20, 100, 50, 30);
+    [btn setTitle:@"时分" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    btn.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:btn];
+    
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake(20, 60, 250, 30)];
+    [self.view addSubview:self.label];
 }
+
+-(void)btnClick{
+    
+    HYTimePickerView *picker = [[HYTimePickerView alloc] initDatePackerWithStartHour:@"00" endHour:@"24" period:15 selectedHour:@"08" selectedMin:@"13"];
+    picker.delegate = self;
+    [picker show];
+}
+
+-(void)timePickerViewDidSelectRow:(NSString *)time{
+    
+    self.label.text = time;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
